cfb2693ca26d7286d6c0ee489c5b36a4f98297da
santiago102307 <santicc1023@gmail.com>
2022-02-16 20:44:09 -0500

    startup-animation

---
+++ MyWebPage/scripts.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', () => {
+    const contenedor_loader = document.querySelector('.contenedor_loader') 
+    contenedor_loader.style.opacity = 0
+    comtenedor_loader.style.visibility = 'hidden'
+})
